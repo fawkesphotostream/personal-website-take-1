1e66fa9c75c3e721e16099b7c3e202bf280ec402
Divya Agrawal <agrawal.divya22@gmail.com>
2020-07-25 11:48:00 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.body.onkeyup = function(e){
+  if(e.keyCode == 68) {
+    document.body.classList.toggle("fun");
+  }
+}
+
+// document.body.onkeyup = function(e){
+//   console.log(e);
+// }
